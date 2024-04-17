9058f3602f95faf1fcc7542e7618bbbd3e90bae2
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-04-16 22:44:25 -0700

    succeflly run the project

---
+++ getx_prectice/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'movie.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var movie = Movie(name: 'Wanted', ticket: 100).obs;
+  // var count = 0.obs;
+ // final count = Rx<int>0;2
+  // final count = RxInt(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("OBX Example"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Obx(
+                () => Text(
+                  "${movie.value.name}",
+                 // "${movie.name}",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                   // movie.name.value = "Hamza";
+                    movie.value.name = "Hamza Ali";
+                    movie.refresh();
+                    print(movie.value.name);
+
+                  },
+                  child: Text("Change"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
